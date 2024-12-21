ffc88c14e9a9b1a122659be146e4d935e90edade
Miniminiso <minisominiso95@gmail.com>
2024-12-20 22:26:04 -0700

    Child1 (#2)
    * adding branch
    
    * multiplied
    
    * added array
    
    * added array
    
    * adding if function
    
    * ‘if_function’
    
    ---------
    
    Co-authored-by: “Miniminiso” <“minisominiso95@gmail.com”>

--- main.go
+++ main.go
@@ -9,6 +9,49 @@ func main() {
 	fmt.Println(a - b)
 	fmt.Println(a * b)
 	test2()
+	test3()
+	test4()
+	test5()
+}
+func test3() {
+	num := 40
+	if num%2 == 0 {
+		fmt.Println("Number is", num, "even")
+	} else {
+		fmt.Println("Number is", num, "odd")
+
+	}
+}
+func test4() {
+
+	if age := 30; age < 5 { // variable declaring inside the if function, so only if will work
+		fmt.Println("Ticket is  Free")
+	} else if age <= 5 || age <= 22 {
+		fmt.Println("Ticket is 10")
+
+	} else {
+		fmt.Println("Ticket price is 15")
+	}
+	//fmt.Println(age) // it say undefined age because age value is inside the "if" statement
+}
+func test5() {
+	marks := 50 //variable declaring outthe function
+	var grade string
+	if marks < 30 {
+		grade = "fail"
+	} else if marks >= 30 && marks <= 50 {
+		grade = "pass"
+	} else {
+		grade = "distint"
+	}
+
+	if marks < 60 {
+		fmt.Println("send message to parents")
+	} else {
+		fmt.Println("no message needed")
+	}
+	fmt.Printf("grade is %T %s\n", grade, grade)
+
 }
 
 func test2() {
